3caa1f59c6a960cbfbf568fefd473e60e61aef5f
Guillaume Gobbe <ggobbe@users.noreply.github.com>
2021-03-15 22:14:16 +0100

    Setup config to publish on npm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+#!/usr/bin/env node
+
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { commands } from './cmds/index.mjs';
 import kafka from './kafka/kafka.mjs';
+
+if(!process.env.KAFKACLI_BROKERS) {
+    console.warn(`To configure kafka-cli, set the following environment variables (cf. README.md):
+    - KAFKACLI_BROKERS
+    - KAFKACLI_SSL (optional)
+    - KAFKACLI_SASL_MECHANISM (optional)
+    - KAFKACLI_SASL_USERNAME (optional)
+    - KAFKACLI_SASL_PASSWORD (optional)
+    - KAFKACLI_SCHEMA_REGISTRY (optional)`);
+      process.exit(-1);
+}
 
 yargs(hideBin(process.argv))
     .scriptName('kafka-cli')
