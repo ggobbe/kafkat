32f3626179830cdef61413f79b370bcb78163aa3
Guillaume Gobbe <guillaume.gobbe@letsbuild.com>
2021-04-12 22:01:45 +0200

    wip-retention

--- src/cmds/topic_cmds/create.mjs
+++ src/cmds/topic_cmds/create.mjs
@@ -1,19 +1,26 @@
 import kafka from '../../kafka/kafka.mjs';
 
 export default {
-    command: 'create <name> [--part] [--repl]',
+    command: 'create',
     desc: 'Create topic',
     builder: {},
     handler: async function (argv) {
-        const success = await kafka.createTopic(argv.name, {
-            numPartitions: argv.part || 6,
-            replicationFactor: argv.repl || 1,
-        });
+        // const success = await kafka.createTopic(argv.name, {
+        //     numPartitions: argv.part || 6,
+        //     replicationFactor: argv.repl || 1,
+        // });
 
-        if(success) {
-            console.log(`topic created: ${argv.name}`);
-        } else {
-            console.log(`error on creating topic: ${argv.name}`);
-        }
+        // if(success) {
+        //     console.log(`topic created: ${argv.name}`);
+        // } else {
+        //     console.log(`error on creating topic: ${argv.name}`);
+        // }
+
+        await kafka.setRetentionMs('alpha.projections.documents');
+        await kafka.setRetentionMs('alpha.projections.issues');
+        await kafka.setRetentionMs('alpha.projections.people');
+        await kafka.setRetentionMs('alpha.projections.projects');
+        await kafka.setRetentionMs('alpha.projections.tasks');
+        await kafka.setRetentionMs('alpha.projections.todolists');
     },
 };
